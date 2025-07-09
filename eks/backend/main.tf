ba4a73f8c9db6a77c82c323d4e231f9bc6758462
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-09 17:52:37 +0530

    commit

---
+++ eks/backend/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+ resource "aws_s3_bucket" "my_bucket" {
+  bucket = "terraform-state-bucket"
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+ }
+ resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "TerraformStateLock"
+    Environment = "Dev"
+  }
+ }
