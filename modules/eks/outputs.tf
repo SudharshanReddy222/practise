fad8408b1cdda715dd48ec4c883f187fc491bfb9
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-10 13:19:06 +0530

    commit

---
+++ modules/eks/outputs.tf
@@ -0,0 +1,8 @@
+outputs "cluster_endpoint" {
+  description = "The endpoint of the EKS cluster"
+  value       = aws_eks_cluster.main.endpoint
+}
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.main.name
+}
---
+++ modules/eks/variables.tf
@@ -0,0 +1,28 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+variable "cluster_version" {
+  description = "Version of the EKS cluster"
+  type        = string
+  default     = "1.27"
+}
+variable "vpc_id" {
+  description = "ID of the VPC where the EKS cluster will be created"
+  type        = string
+}
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+variable "node_groups" {
+  description = "Map of node group configurations"
+  type = map(object({
+    instance_type = string
+    desired_capacity = number
+    max_size = number
+    min_size = number
+    key_name = string
+  }))
+  default = {}
+}
