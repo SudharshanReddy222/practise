551180a9579c634b6615faa8c5075ce7da2d20ae
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-10 19:26:30 +0530

    push

--- modules/vpc/main-1.tf
+++ modules/vpc/main-1.tf
@@ -61,7 +61,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
     }
     tags = {
@@ -77,7 +77,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main[0].id
   }
 
