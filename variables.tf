e4c6f19175c4fde274c166da36c1439b51df1588
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-17 19:23:18 +0530

    increaesed the pod number

--- variables.tf
+++ variables.tf
@@ -43,8 +43,8 @@ variable "node_groups" {
   default = {
     eks_nodes = {
       instance_type   = "t3.medium"
-      desired_capacity = 2
-      max_size        = 3
+      desired_capacity = 3
+      max_size        = 4
       min_size        = 1
       key_name        = "terraform-key"
     }
