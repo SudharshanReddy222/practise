bdc65cfd7e4baf3da015a8529f8e31b1569a5916
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-17 19:35:18 +0530

    commit

--- variables.tf
+++ variables.tf
@@ -42,8 +42,8 @@ variable "node_groups" {
   }))
   default = {
     eks_nodes = {
-      instance_type   = "t3.medium"
-      desired_capacity = 3
+      instance_type   = "t3.large"
+      desired_capacity = 2
       max_size        = 4
       min_size        = 1
       key_name        = "terraform-key"
