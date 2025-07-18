bca3230503dbea59482453e68e3b0d8769dc3d91
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-18 12:33:37 +0530

    configured sns topic

--- sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "alarm_topic" {
-  name = "cpu-util-alarm-topic"
-}
-
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.alarm_topic.arn
-  protocol  = "email"
-  endpoint  = "var.sns_email"  # Replace with your email address
-}
