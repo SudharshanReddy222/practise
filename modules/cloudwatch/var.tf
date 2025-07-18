d5f66925917553dea6f29dfe4f1048175f37fade
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-18 13:15:36 +0530

    commit

--- modules/cloudwatch/var.tf
+++ modules/cloudwatch/var.tf
@@ -22,22 +22,3 @@ variable "ok_actions" {
   description = "List of actions (SNS topic ARNs) to invoke when alarm returns to OK"
   type        = list(string)
 }
-variable "dashboard_name" {
-  description = "Name of the CloudWatch dashboard"
-  type        = string
-}
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "EKS Cluster name"
-  type        = string
-}
-
-variable "nodegroup_name" {
-  description = "EKS Node Group name"
-  type        = string
-}
