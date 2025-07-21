f415b1d108e3102bdd3c7f028d7daea2af0f4506
SudharshanReddy222 <sudharshangangireddy@gmail.com>
2025-07-21 12:26:56 +0530

    commit

--- backend/backend.tf
+++ backend/backend.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
  resource "aws_s3_bucket" "my_bucket" {
-  bucket = "terraform-state-bucket-934746"
+  bucket = "terraform-state-bucket-93474676"
   lifecycle {
     prevent_destroy = true
   }
